e36aa2cdb108cd77ee087c9c23a9ccf0396b40ce
HichamHajla <hichem500@hotmail.com>
2023-01-03 16:05:13 +0100

    add react moment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import './App.css';
 import axios from 'axios';
-import Button from '@mui/material/Button';
 import News from './News';
 
 const App = () => {
 
   const [data, setData] = useState([])
   //const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    fetchData();
+  }, [])
 
   const fetchData = () => {
     axios.get(`http://hn.algolia.com/api/v1/search?query=`)
@@ -20,14 +23,16 @@ const App = () => {
 
   return (
     <div className="app">
-    <Button onClick={()=> fetchData()} variant="contained">Call data</Button>
-
     <div className="navbar">
+    <img className="logo" src="https://upload.wikimedia.org/wikipedia/commons/b/b2/Y_Combinator_logo.svg" alt="logo"/>
+    <h3 className='title'>Hacker News</h3>
+    <p className="links"><a href="#new">new</a> | <a href="#past">past</a> | <a href="#comments">comments</a> | <a href="#ask">ask</a> | <a href="#show">show</a> | <a href="#jobs">jobs</a> | <a href="#submit">submit</a> <a className="login" href="#login">login</a> </p>
     </div>
     <div className="news">
+
     <ol>
-    {data.map((element, index) => {
-      return(
+        {data.map((element, index) => {
+        return(
         <div key={index}>
         
         <News {...element}/>
@@ -36,9 +41,14 @@ const App = () => {
       )
     })}
     </ol>
+
     </div> 
+
     <footer>
+    <div className='footerLinks'><a href="#Guidelines">Guidelines</a> | <a href="#FAQ">FAQ</a> | <a href="#API">comments</a> | <a href="#Security">Security</a> | <a href="#Legal">Legal</a> | <a href="#Apply">Apply to YC</a> | <a href="#contact">Contact</a></div>
+    <div className='search'></div>
     </footer>
+
     </div>
   );
 }
--- src/News.js
+++ src/News.js
@@ -1,12 +1,15 @@
 
+import Moment from 'react-moment';
+
 const News = ({title, url, points, author, created_at, num_comments}) =>{
+    if (title !== null){
     return(
         <div>
         <li><p><b>{title}</b> ({url})</p>
-            <p>{points} points by {author} {created_at} | hide | {num_comments} comments</p>
+            <p>{points} points by {author} <Moment fromNow>{created_at}</Moment> | hide | {num_comments} comments</p>
         </li>
         </div>
-    )
+    )}
 }
 
 
