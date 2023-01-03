986a397ea4146c1f4e707c299aba69a6aeb9e546
HichamHajla <hichem500@hotmail.com>
2023-01-03 22:43:42 +0100

    added some css, changed colors and added some links

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ const App = () => {
     <div className="navbar">
     <img className="logo" src="https://upload.wikimedia.org/wikipedia/commons/b/b2/Y_Combinator_logo.svg" alt="logo"/>
     <h3 className='title'>Hacker News</h3>
-    <p className="links"><a href="#new">new</a> | <a href="#past">past</a> | <a href="#comments">comments</a> | <a href="#ask">ask</a> | <a href="#show">show</a> | <a href="#jobs">jobs</a> | <a href="#submit">submit</a> <a className="login" href="#login">login</a> </p>
+    <p className="links"><a href="#new">new</a> | <a href="#past">past</a> | <a href={"https://news.ycombinator.com/newcomments"}>comments</a> | <a href="#ask">ask</a> | <a href="#show">show</a> | <a href="#jobs">jobs</a> | <a href="#submit">submit</a> <a className="login" href="#login">login</a> </p>
     </div>
     <div className="news">
 
